4e3fe397073e4d8aeba80c3f184088e4277370c6
Ernad Husremovic <hernad@bring.out.ba>
2015-12-14 18:44:27 +0100

    upload_app

---
+++ upload_app.sh
@@ -0,0 +1,34 @@
+BINTRAY_API_KEY=`cat bintray_api_key`
+BINTRAY_REPOS=greenbox
+
+DOCKER_VERSION=`cat DOCKER_VERSION`
+#BINTRAY_PACKAGE=VirtualBox
+BINTRAY_PACKAGE=$1
+#BINTRAY_PACKAGE_VER=5.0.10
+BINTRAY_PACKAGE_VER=$2
+
+
+[ -z "$BINTRAY_PACKAGE" ] && echo package name mora biti navedeno && exit 1
+[ -z "$BINTRAY_PACKAGE_VER" ] && echo package version mora biti navedeno && exit 1
+
+docker rm -f greenbox
+docker run --name greenbox greenbox:$DOCKER_VERSION /bin/false
+docker cp greenbox:/opt/apps/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} 
+
+FILE=${BINTRAY_PACKAGE}_${BINTRAY_PACKAGE_VER}.tar.gz
+
+tar cvfz $FILE ${BINTRAY_PACKAGE}
+rm -r -f ${BINTRAY_PACKAGE}
+
+ls -lh $FILE
+
+echo uploading to bintray ...
+
+curl -T $FILE \
+      -u hernad:$BINTRAY_API_KEY \
+     https://api.bintray.com/content/hernad/greenbox/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/$FILE
+
+curl -u hernad:$BINTRAY_API_KEY \
+   -X POST https://api.bintray.com/content/hernad/greenbox/$BINTRAY_PACKAGE/$BINTRAY_PACKAGE_VER/publish
+
+
