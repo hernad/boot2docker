b511dde250b1e012a0de8f4e4c67d0b507c27598
Ernad Husremovic <hernad@bring.out.ba>
2016-01-04 13:48:25 +0100

    fix error

--- upload_app.sh
+++ upload_app.sh
@@ -12,13 +12,15 @@ BINTRAY_PACKAGE_VER=$2
 [ -z "$BINTRAY_PACKAGE_VER" ] && echo package version mora biti navedeno && exit 1
 
 FILE=${BINTRAY_PACKAGE}_${BINTRAY_PACKAGE_VER}.tar.gz
+CT=greenbox_apps
 
 if [ ! -f $FILE ] ; then
    case ${BINTRAY_PACKAGE} in
-      VirtualBox|vagrant)
-           docker rm -f greenbox
-           docker run --name greenbox greenbox:$DOCKER_VERSION ls /opt/apps && rm -rf VirtualBox
-           docker cp greenbox:/opt/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
+      VirtualBox)
+           CT=greenbox
+           docker rm -f $CT
+           docker run --name $CT $CT:$DOCKER_VERSION ls /opt/apps && rm -rf VirtualBox
+           docker cp $CT:/opt/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
            chmod +s VirtualBox/VirtualBox VirtualBox/VBoxHeadless &&\
            find VirtualBox/src -type f -exec rm  {} \; &&\
            find VirtualBox/ExtensionPacks/Oracle_VM_VirtualBox_Extension_Pack/solaris.amd64 -type f -exec rm {} \; &&\
@@ -29,15 +31,15 @@ if [ ! -f $FILE ] ; then
            find VirtualBox -name "*.o" -exec rm {} \; &&\
            find VirtualBox -name "*.c" -exec rm {} \; || exit 1
            ;;
-      flocker)
-           docker rm -f greenbox_apps
-           docker run --name greenbox_apps greenbox_apps:$DOCKER_VERSION ls /opt/flocker
-           docker cp greenbox_apps:/opt/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
+      flocker|vagrant)
+           docker rm -f $CT
+           docker run --name $CT $CT:$DOCKER_VERSION find /opt/${BINTRAY_PACKAGE}
+           docker cp $CT:/opt/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
            ;; 
       *) 
-           docker rm -f greenbox_apps
-           docker run --name greenbox_apps greenbox_apps:$DOCKER_VERSION ls /opt/apps
-           docker cp greenbox_apps:/opt/apps/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
+           docker rm -f $CT
+           docker run --name $CT $CT:$DOCKER_VERSION ls /opt/apps
+           docker cp $CT:/opt/apps/${BINTRAY_PACKAGE} ${BINTRAY_PACKAGE} || exit 1
            [ ! -d ${BINTRAY_PACKAGE}/sbin ] ||  mv ${BINTRAY_PACKAGE}/sbin/*  ${BINTRAY_PACKAGE}/bin/ 
            if  [ -d bins/${BINTRAY_PACKAGE} ]
            then
