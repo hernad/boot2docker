05433ba684927f43dc330de80dd80925e0479563
Ernad Husremovic <hernad@bring.out.ba>
2016-01-05 15:01:28 +0100

    vbox deploy

---
+++ vbox/vbox_deploy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+# Thank you githib.com/RackHD/RackHD
+
+SCRIPT_DIR=$(cd $(dirname $0) && pwd)
+source $SCRIPT_DIR/deploy.cfg
+
+
+function usage {
+  printf "\nusage: vbox_deploy [-h]\n"
+  printf "\t-h display usage\n\n"
+  printf "\t customize deployment variables by editing:\n"
+  printf "\t\t deploy.cfg\n\n"
+  exit
+}
+
+
+function vbox_create {
+
+vmName=$1
+vmDiskSize=$2
+
+echo "deploying machine: $vmName"
+
+if [ -e $vmName.vdi ]
+then
+   echo $vmName.vdi exists
+   return
+fi
+
+VBoxManage createvm --name $vmName --register
+VBoxManage modifyvm $vmName --ostype Linux_64  --memory 768
+VBoxManage modifyvm $vmName --nic1 intnet --intnet1 greennet --nicpromisc1 allow-all
+#VBoxManage modifyvm $vmName --nictype1 82540EM --macaddress1 auto
+VBoxManage modifyvm $vmName --nictype1 virtio --macaddress1 auto
+
+VBoxManage createhd --filename $vmName --size $vmDiskSize
+#VBoxManage modifyvm $vmName --ostype Ubuntu --boot1 net --memory 768;
+
+VBoxManage storagectl $vmName --name "IDE Controller" --add ide
+VBoxManage storageattach $vmName --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "greenbox.iso"
+
+VBoxManage storagectl $vmName --name "SATA Controller" --add sata --controller IntelAHCI
+VBoxManage storageattach $vmName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $vmName.vdi
+
+ 
+}
+
+while getopts ":h" opt; do
+  case $opt in
+    h)
+      usage
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      ;;
+  esac
+done
+
+
+#echo "I'll set up monorail server now..."
+#vagrant up dev
+
+vmDiskSize=16384
+
+if [ $machine_count ]
+  then
+    for (( i=1; i <= $machine_count; i++ ))
+      do
+        vmName="green-vbox-$i"
+        vbox_create $vmName $vmDiskSize
+      done
+fi
+
+echo end
+#vagrant ssh dev -c "sudo nf start"
