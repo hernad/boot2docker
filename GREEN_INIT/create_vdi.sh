196d6dfef93c473b3aa3da54f9ee705ff163c1f4
Ernad Husremovic <hernad@bring.out.ba>
2016-01-06 08:42:32 +0100

    fixes

--- GREEN_INIT/create_vdi.sh
+++ GREEN_INIT/create_vdi.sh
@@ -1,9 +1,13 @@
+#!/bin/bash
 
-
-#vagrant scp default:/vagrant/GREEN_INIT.vmdk .
+vagrant scp default:/vagrant/GREEN_INIT.vmdk .
 
 rm GREEN_INIT.vdi
 
 VBoxManage clonehd GREEN_INIT.vmdk  GREEN_INIT.vdi
 
 VBoxManage closemedium GREEN_INIT.vdi
+
+rm GREEN_INIT.vmdk
+
+
