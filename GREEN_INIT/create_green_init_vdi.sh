1a3c92c78a9a6d5907c8daef18629e7be4fbc69b
Ernad Husremovic <hernad@bring.out.ba>
2016-03-06 21:31:12 +0100

    green_init

--- GREEN_INIT/create_green_init_vdi.sh
+++ GREEN_INIT/create_green_init_vdi.sh
@@ -2,13 +2,17 @@
 
 if [[ "$1" == "--create-tgz" ]] ; then
    tar -cvf green_init.tar.gz  -C data  . 
+   vagrant resync
 fi
 
 vagrant up # create default vdi
 
+vagrant ssh -c "ls -l /vagrant/green_init.tar.gz"
+
 if [ ! -f GREEN_INIT.vmdk ] ; then
-   vagrant provision  # vmdk not created in previous step
+   vagrant reload --provision   # vmdk not created in previous step
 fi
+
 
 if [ -f GREEN_INIT.vmdk ] ; then
    vagrant scp default:/vagrant/GREEN_INIT.vmdk .
