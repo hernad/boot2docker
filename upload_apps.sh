7420273a597332a8e16afa0bb9f0c8d2559039fd
Ernad Husremovic <hernad@bring.out.ba>
2016-09-21 15:52:38 +0200

    vim VirtualBox green

---
+++ upload_apps.sh
@@ -0,0 +1,16 @@
+VER=5.1.6
+KERNEL=4.4.21
+APP=VirtualBox
+#./upload_app.sh $APP ${VER}-${KERNEL}
+
+APP=green
+VER=1.6.0
+#./upload_app.sh $APP $VER
+
+APP=vagrant
+VER=1.8.5
+./upload_app.sh $APP $VER
+
+APP=vim
+VER=8.0.0
+./upload_app.sh $APP $VER
