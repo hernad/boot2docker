2a5f4925373c01899a3b0704639291f6b835e21d
Ernad Husremovic <hernad@bring.out.ba>
2016-11-02 18:22:36 +0100

    vagrant vm destroy on start-end

--- GREEN_INIT/create_green_init_vdi.sh
+++ GREEN_INIT/create_green_init_vdi.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-vagrant up # create default vdi
+
+( vagrant status | grep -q "not created" ) || ( echo "destroying vm" && vagrant destroy --force )
+
+
+vagrant destroy --force 
+
+for ext in "vmdk img vdi" ; do
+  [ -f GREEN_INIT.$ext ]  || rm GREEN_INIT.$ext
+done
 
 
 if [[ "$1" == "--create-tgz" ]] ; then
    tar -cvf green_init.tar.gz  -C data  . 
-   vagrant rsync
 fi
 
+
+vagrant up # create default vdi
 
 vagrant ssh -c "ls -l /vagrant/green_init.tar.gz"
 
-if [ ! -f GREEN_INIT.vmdk ] ; then
-   vagrant reload --provision   # vmdk not created in previous step
-fi
-
+#if [ ! -f GREEN_INIT.vmdk ] ; then
+#   vagrant reload --provision   # vmdk not created in previous step
+#fi
 
 vagrant scp default:/vagrant/GREEN_INIT.vmdk .
 
@@ -23,4 +31,7 @@ VBoxManage clonehd GREEN_INIT.vmdk  GREEN_INIT.vdi
 
 VBoxManage closemedium GREEN_INIT.vdi
 rm GREEN_INIT.vmdk
+
+
+( vagrant status | grep -q running ) && echo destroying && vagrant destroy --force
 
