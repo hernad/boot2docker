c0fa177b6b3789aa29ab619eb0986afd1f779364
Ernad Husremovic <hernad@bring.out.ba>
2016-11-03 08:41:57 +0100

    fix

--- rootfs/rootfs/etc/profile.d/green_profile.sh
+++ rootfs/rootfs/etc/profile.d/green_profile.sh
@@ -26,15 +26,15 @@ if ps ax | grep -q curl.*.tar.gz$ ; then
 fi
 echo -e
 echo_line " Application versions: "
-[ -n "`which vim`" ] echo "`vim --version | grep ^VIM`"
+[ -n "`which vim`" ] && echo "`vim --version | grep ^VIM`"
 [ -e /usr/bin/python ] && echo "`/usr/bin/python --version`"
 [ -e /usr/bin/perl ] && echo "`perl -v | sed -n '/This is perl/,2p'`"
 [ -e /opt/VirtualBox/VirtualBox ] && echo "VirtualBox: `VBoxManage --version`"
 [ -d /opt/go ] && export GOROOT=/opt/go && /opt/go/bin/go version
 [ -n "$GOROOT" ] && export GOPATH=/home/docker/go && mkdir -p $GOPATH && echo "GOPATH=$GOPATH"
-[ -n "`which npm`"] && echo "nodejs/npm: `npm version`"
+[ -n "`which npm`" ] && echo "nodejs/npm: `npm version`"
 echo -e
-echo_line " Kernel info: "
+echo_line "    Kernel info:   "
 uname -a
 echo "ZFS `modinfo zfs | grep "version.*[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]]\+\.[[:digit:]-]\+$"`"
 echo -e
