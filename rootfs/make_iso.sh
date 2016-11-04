7df79705345b4761a20df78660b17f973fe46ded
Ernad Husremovic <hernad@bring.out.ba>
2016-11-04 07:47:28 +0100

    xauth, green/gdrive

--- rootfs/make_iso.sh
+++ rootfs/make_iso.sh
@@ -28,12 +28,12 @@ ln -fs $BOOT_DIR/etc/dnsmasq.conf $ROOTFS/etc/
 # /bin/bash
 ln -fs /usr/local/bin/bash $ROOTFS/bin/
 
-# symlinks /usr/bin for ansible
+
 # /usr/bin/python, /usr/bin/perl, /usr/bin/logrotate
-ln -fs /opt/python2/bin/python $ROOTFS/usr/bin/
+ln -fs /opt/python2/bin/python $ROOTFS/usr/bin/ ; ln -fs /opt/python2/lib/libpython2.7.so.1.0 $ROOTFS/usr/lib/ # symlinks /usr/bin for ansible
 ln -fs /opt/green/bin/logrotate $ROOTFS/usr/bin/
-ln -fs /opt/python2/lib/libpython2.7.so.1.0 $ROOTFS/usr/lib/
 ln -fs /opt/perl5/bin/perl $ROOTFS/usr/bin/
+ln -fs /opt/x11/bin/xauth $ROOTFS/usr/bin/
 
 
 # Setup /etc/os-release with some nice contents
