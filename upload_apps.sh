4b4a2d18df446ea9eb1781c0fe80f42d3ad88aff
Ernad Husremovic <hernad@bring.out.ba>
2016-11-05 18:58:06 +0100

    x11 drm_ exclude

--- upload_apps.sh
+++ upload_apps.sh
@@ -1,50 +1,61 @@
 VER=5.1.8
 APP=VirtualBox
-./upload_app.sh $APP ${VER} J  #.tar.xz
+#./upload_app.sh $APP ${VER} J  #.tar.xz
 
 APP=green
-VER=1.7.1
+VER=2.0.0
+rm -rf $APP
 ./upload_app.sh $APP $VER  J
 
 APP=vagrant
 VER=1.8.6
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=vim
 VER=8.0.5
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=python2
 VER=2.7.12
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 
 APP=go
 VER=1.7.3
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 
 APP=node
 VER=6.9.1
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=x11
-VER=3.0.1
+VER=3.1.0
+rm -rf $APP
 ./upload_app.sh $APP $VER J
 
 
 APP=java
 VER=8.112.15
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=atom
 VER=1.11.2
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=code
 VER=1.7.1
-./upload_app.sh $APP $VER J
+rm -rf $APP
+#./upload_app.sh $APP $VER J
 
 APP=idea
 VER=2016.5.2
-#./upload_app.sh $APP $VER J 
+rm -rf $APP
+#./upload_app.sh $APP $VER J
