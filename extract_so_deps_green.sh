359ef05f4c97475dab1bd456338955025607e16f
Ernad Husremovic <hernad@bring.out.ba>
2016-11-08 11:26:48 +0100

    parted so libs

---
+++ extract_so_deps_green.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+
+GREEN_LIB=/opt/apps/green/lib/
+mkdir $GREEN_LIB
+
+LDD_CHECK="/opt/apps/green/bin/parted"
+
+
+for ldd_f in $LDD_CHECK ; do
+
+   echo "=============================== $ldd_f ============================================="
+   FILES_FULLPATH_0=`ldd $ldd_f | awk '{print $1 $2 $3 $4}'`
+   FILES_FULLPATH=`ldd $ldd_f | awk '{print $3 }'`
+   FILES=`ldd $ldd_f | awk '{print $3 }' | sed -e 's/^.*\/\(lib\([xXa-zA-Z0-9._+]\|-\)\+\)$/\1/'`
+
+   for f in $FILES_FULLPATH_0 ; do
+        if [[ "$f" =~ "notfound" ]] ; then
+            echo ">>>>>>>>>>>>>>>> ERROR $f <<<<<<<<<<<<<<<<<<<<<<<<"
+        fi
+   done
+
+   for f in $FILES ; do
+
+        echo "$f"
+
+        f_rootfs=`find /rootfs -name $f`
+        if [ -z "$f_rootfs" ] ; then
+            echo "$f nema u rootfs"
+
+            for fp in $FILES_FULLPATH ; do
+               [ -n "$f" ] && [[ "$fp" =~ "$f" ]]  && echo kopirati $fp && cp -Lv $fp $GREEN_LIB  # derefrence symlinks
+            done
+        fi
+    done
+
+done
