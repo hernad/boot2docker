4c539262c37b10f842b45c7468d2ed2a482bd5a7
Ernad Husremovic <hernad@bring.out.ba>
2016-12-11 13:00:01 +0100

    vbox zpool auto create

--- rootfs/rootfs/usr/local/bin/vbox_create_pool.sh
+++ rootfs/rootfs/usr/local/bin/vbox_create_pool.sh
@@ -6,20 +6,18 @@ POOL=green
 . /etc/green_common
 
 fdisk_exist () {
-
 fdisk -l /dev/${DISK} | grep -q "/dev/$1"
-
 }
 
 if ( ! is_vbox )
 then
-   echo "mora biti vbox" 
+   log_msg "automatsko kreiranje zpool-a: mora biti vbox"
    exit 1
 fi
 
 if ( zpool list | grep -q $POOL )
 then
-  echo "zpool green vec postoji"
+  echo "zpool green already exists"
   exit 0
 fi
 
@@ -28,21 +26,13 @@ fdisk -l /dev/$DISK >> $LOG_FILE
 
 if ( ! fdisk_exist ${DISK}1 )
 then
-  echo create $DISK partition 1
+  log_msg "create $DISK partition 1"
   #n p 1 1 <enter> w
   (echo n; echo p; echo 1; echo 1; echo ; echo w) | sudo fdisk /dev/${DISK}
+  log_msg "zpool create $POOL na /dev/${DISK}1"
+  zpool create -f $POOL /dev/${DISK}1
+  zpool list >> $LOG_FILE
 else
-  echo ${DISK} partition 1 exists
+  log_msg "${DISK} partition 1 already exists"
   exit 1
 fi
-
-if ( fdisk_exist ${DISK}1 ) 
-then
-   echo kreiram zpool $POOL na /dev/${DISK}1
-   zpool create -f $POOL /dev/${DISK}1
-else
-   echo "${DISK} partition 1 doesn't exists"
-fi
-
-zpool list >> $LOG_FILE
-
