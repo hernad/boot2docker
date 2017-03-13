72308ea686cf2d283cfb82dfb1fd7a381412c40b
Linux User <Ernad husremovic>
2017-03-13 16:29:20 +0100

    update

--- upload_apps.sh
+++ upload_apps.sh
@@ -1,11 +1,11 @@
 VER=5.1.16
 APP=VirtualBox
-./upload_app.sh $APP ${VER} J  #.tar.xz
+#./upload_app.sh $APP ${VER} J  #.tar.xz
 
 APP=docker
 VER=`cat DOCKER_VERSION`
 rm -rf $APP
-./upload_app.sh $APP $VER  J
+#./upload_app.sh $APP $VER  J
 
 APP=green
 VER=3.1.0
@@ -18,7 +18,7 @@ rm -rf $APP
 #./upload_app.sh $APP $VER J
 
 APP=vagrant
-VER=1.9.1
+VER=1.9.2
 rm -rf $APP
 ./upload_app.sh $APP $VER J
 
