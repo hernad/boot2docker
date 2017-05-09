65047f9cfae61e28732bb0baae03c199ff3f9b58
hernad <hernad@bring.out.ba>
2017-05-09 08:30:20 +0200

    parted is not in iso, use fdisk

--- rootfs/rootfs/usr/local/bin/scaleway_create_pool.sh
+++ rootfs/rootfs/usr/local/bin/scaleway_create_pool.sh
@@ -6,34 +6,12 @@ POOL=green
 
 . /etc/green_common
 
-scaleway_server () {
+echo "${GREEN}scaleway create pool.sh start${NORMAL}"
 
-#parted /dev/vda  print
-#WARNING: You are not superuser.  Watch out for permissions.
-#Model: Virtio Block Device (virtblk)
-#Disk /dev/vda: 50.0GB
-
-parted /dev/vda print | grep -q "/dev/$1.*GB"
-if [ "$?" != "0" ]; then
-   return 1
-fi
-
-#initrd=initrd showopts console=ttyS0,115200 nousb vga=0 root=/dev/vda ip=:::::eth0: boot=local
-cat /proc/cmdline | grep -q "root=/dev/vda"
-if [ "$?" != "0" ]; then
-   return 1
-fi
-
-
-[ -d /run ] || mkdir /run
-curl http://$SCALEWAY_SIGNAL_SERVER/conf -X GET > /run/scw-metadata.cache
-cat /run/scw-metadata.cache  | grep PUBLIC_IP_ADDRESS
-
-}
 
 if ( ! scaleway_server )
 then
-   log_msg "automatsko kreiranje zpool-a: mora biti scaleway"
+   log_msg "automatsko kreiranje zpool-a: scaleway only"
    exit 1
 fi
 
@@ -43,8 +21,7 @@ then
   exit 0
 fi
 
-
-parted /dev/$DISK print >> $LOG_FILE
+fdisk /dev/vda -l | grep GB >> $LOG_FILE
 
 zpool create -f $POOL /dev/${DISK}
 zpool list >> $LOG_FILE
