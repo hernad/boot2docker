d55ebefd046927c6321c0558a0d50f3d62adb2bd
hernad <hernad@bring.out.ba>
2017-05-15 11:52:31 +0200

    fix touch .download

--- build.sh
+++ build.sh
@@ -59,7 +59,7 @@ if [ -f docker_password ] ; then ## if file docker_password exists set dockerpwd
 fi
 
 # lang=bs_BA.UTF-8"
-# ISO_APPEND+=" secure rootpwd=root01"
+# ISO_APPEND+=" rootpwd=root01"
 ISO_APPEND+=" tz=CET-1CEST,M3.5.0,M10.5.0\/3"
 ISO_APPEND+=" noembed nomodeset norestore waitusb=10 LABEL=GREEN_HDD"
 
