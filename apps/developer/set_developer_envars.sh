b99c5ac4d5c85f6b5c91a7a6fafb5dbb24059006
hernad <hernad@bring.out.ba>
2017-05-16 13:01:14 +0200

    set_developer_envars.sh

---
+++ apps/developer/set_developer_envars.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "=========== set developer envars ======"
+echo -e
+echo  "RUN cmd: \$ source $0 ====="
+
+CFLAGS="-I/opt/apps/green/include -I/opt/apps/python2/include"
+CFLAGS+=" -I/opt/apps/developer/include "
+#linux includes
+CFLAGS+=" -I/opt/apps/developer/include/linux/x86/include -I/opt/apps/developer/include/linux/x86/include/generated"
+CFLAGS+=" -I/opt/apps/developer/include/linux/x86/include/uapi -I/opt/apps/developer/include/linux/x86/include/generated/uapi"
+CFLAGS+=" -I/opt/apps/developer/include/linux/include -I/opt/apps/developer/include/linux/include/uapi"
+
+CPPFLAGS="$CFLAGS"
+LDFLAGS="-L/opt/apps/green/lib -L/opt/apps/python2/lib -L/opt/apps/developer/lib"
+
+export CFLAGS CPPFLAGS LDFLAGS
+
+
+echo CFLAGS=$CFLAGS
+echo CPPFLAGS=$CPPFLAGS
+echo LDFLAGS=$LDFLAGS
