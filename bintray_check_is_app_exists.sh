3cca1fb554a2bdb1d4a2a9a04aceddda3d4e4dd1
hernad <hernad@bring.out.ba>
2017-05-17 09:17:39 +0200

    bintray app

--- bintray_check_is_app_exists.sh
+++ bintray_check_is_app_exists.sh
@@ -8,11 +8,8 @@ BINTRAY_API_KEY=${BINTRAY_API_KEY:-`cat bintray_api_key`}
 BINTRAY_USER=hernad
 BINTRAY_REPOS=greenbox
 
-echo "bintray check $APP_$VER.tar.xz file exists"
+echo "bintray ${APP} check $APP_$VER.tar.xz file exists"
 curl -s  -u hernad:$BINTRAY_API_KEY \
       --header "X-Bintray-Override: 1" \
       -X GET https://api.bintray.com/file_version/$BINTRAY_USER/$BINTRAY_REPOS/${APP}_${VER}.tar.xz \
       | grep $VER
-
-
-
