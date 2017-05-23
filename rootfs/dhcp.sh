4268cd6eebd8c3008bae1f24d31d85ed5a232e5b
hernad <hernad@bring.out.ba>
2017-05-23 10:22:27 +0200

    fix

--- rootfs/dhcp.sh
+++ rootfs/dhcp.sh
@@ -10,15 +10,15 @@ log_msg "dhcp - udhcpc start"
 /sbin/udevadm settle --timeout=5 # This waits until all devices have registered
 
 NETDEVICES="$(awk -F: '/eth.:|tr.:/{print $1}' /proc/net/dev 2>/dev/null)"
-
-log_msg "network devices: $NETDEVICES" M
+HOSTNAME="$(/bin/hostname)"
+log_msg "HOSTNAME: $HOSTNAME network devices: $NETDEVICES" M
 
 for DEVICE in $NETDEVICES; do
   ifconfig $DEVICE | grep -q "inet addr"
   if [ "$?" != 0 ]; then
     log_msg "Network device $DEVICE detected, DHCP broadcasting for IP"
     trap 2 3 11
-    /sbin/udhcpc -b -i $DEVICE -x hostname:$(/bin/hostname) \
+    /sbin/udhcpc -b -i $DEVICE -x hostname:$HOSTNAME \
        -s /usr/share/udhcpc/default.script \
        -p /var/run/udhcpc.$DEVICE.pid >/dev/null 2>&1 &
     trap "" 2 3 11
