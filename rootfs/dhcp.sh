0c822b2a7661225f0267b42be897f4438654b9b3
hernad <hernad@bring.out.ba>
2017-05-24 12:14:07 +0200

    x3x50M4_server bonding

--- rootfs/dhcp.sh
+++ rootfs/dhcp.sh
@@ -9,7 +9,24 @@ log_msg "dhcp - udhcpc start"
 
 /sbin/udevadm settle --timeout=5 # This waits until all devices have registered
 
-NETDEVICES="$(awk -F: '/eth.:|tr.:/{print $1}' /proc/net/dev | sort 2>/dev/null)"
+if is_x3x50M4_server ; then
+  # eth0, eth1, eth2 bonding
+  NETDEVICES="bond0 eth3" 
+  modprobe bonding
+  ifconfig bond0 up
+  ifconfig eth0 down
+  ifconfig eth1 down
+  ifconfig eth2 down
+  echo "+eth0" > /sys/class/net/bond0/bonding/slaves
+  echo "+eth1" > /sys/class/net/bond0/bonding/slaves
+  echo "+eth2" > /sys/class/net/bond0/bonding/slaves
+  log_msg "x3x50M4_server bond0 `cat /sys/class/net/bond0/bonding/mode`" M
+
+else
+  NETDEVICES="$(awk -F: '/eth.:|tr.:/{print $1}' /proc/net/dev | sort 2>/dev/null)"
+fi
+
+
 HOSTNAME="$(/bin/hostname)"
 log_msg "HOSTNAME: $HOSTNAME" M
 
