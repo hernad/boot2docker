e7d7f8b5364f42ca8bda8eddd53440bd7aa14047
hernad <hernad@bring.out.ba>
2017-05-24 23:09:20 +0200

    dns

--- rootfs/dhcp.sh
+++ rootfs/dhcp.sh
@@ -11,15 +11,14 @@ log_msg "dhcp - udhcpc start"
 
 if is_x3x50M4_server ; then
   # eth0, eth1, eth2 bonding
-  NETDEVICES="bond0 eth3"
+  NETDEVICES="bond0"
   modprobe bonding
-  ifconfig bond0 up
   ifconfig eth0 down
   ifconfig eth1 down
   ifconfig eth2 down
-  echo "+eth0" > /sys/class/net/bond0/bonding/slaves
-  echo "+eth1" > /sys/class/net/bond0/bonding/slaves
-  echo "+eth2" > /sys/class/net/bond0/bonding/slaves
+  ifconfig eth3 down
+  ifconfig bond0 up
+  echo "+eth0 +eth1 +eth2 +eth3" > /sys/class/net/bond0/bonding/slaves
   log_msg "x3x50M4_server bond0 `cat /sys/class/net/bond0/bonding/mode`" M
 
 else
