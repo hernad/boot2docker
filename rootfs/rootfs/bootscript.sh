3c03aabc9f6e6a0e82fb56c61feb76089a8c63d5
hernad <hernad@bring.out.ba>
2017-05-25 11:43:52 +0200

    coredns proxy dns

--- rootfs/rootfs/bootscript.sh
+++ rootfs/rootfs/bootscript.sh
@@ -167,7 +167,7 @@ fi
 [ -d $BOOT_DIR/root ] || mkdir -p $BOOT_DIR/root
 [ -d /root ] && mv /root /root.orig
 ln -s $BOOT_DIR/root /root
-[ -d /root.orig ]  && mv /root.orig/* /root/ && rm -rf /root.orig
+[ -d /root.orig ] && mv /root.orig/* /root/ && rm -rf /root.orig
 
 echo "${GREEN}KERNEL cmdline:${NORMAL}  `cat /proc/cmdline`"
 
