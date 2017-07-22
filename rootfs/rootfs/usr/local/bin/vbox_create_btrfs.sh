2873311e91acf49db6a97c695747ed583a07f6e3
hernad <hernad@bring.out.ba>
2017-07-22 18:30:46 +0200

    btrfs mount

--- rootfs/rootfs/usr/local/bin/vbox_create_btrfs.sh
+++ rootfs/rootfs/usr/local/bin/vbox_create_btrfs.sh
@@ -2,17 +2,26 @@
 
 . /etc/green_common
 
-if ( ! is_vbox )
-then
-   log_msg "automatsko kreiranje btrfs: mora biti vbox"
-   exit 1
-fi
-
-DISK=/dev/sda
-
 zfs_partition_sda_exists() {
    /sbin/fdisk $DISK -l | grep -q 0700
 }
+
+mount_green_btrfs() {
+ log_msg "mounting btrfs /green-btrfs on disk $DISK" Y
+ mkdir -p /green-btrfs
+ # btrfs device scan is used to scan all of the block devices under /dev and probe for Btrfs volumes. This is required after loading the btrfs module if you're running with more than one device in a filesystem.
+ btrfs device scan
+ mount $DISK /green-btrfs
+}
+
+DISK=/dev/sda
+
+if ( ! is_vbox )
+then
+   mount_green_btrfs
+   exit 0
+fi
+
 
 if zfs_partition_sda_exists
 then
@@ -32,7 +41,4 @@ fi
 
 btrfs inspect-internal dump-super ${DISK}  >> $LOG_FILE
 
-mkdir -p /green-btrfs
-# btrfs device scan is used to scan all of the block devices under /dev and probe for Btrfs volumes. This is required after loading the btrfs module if you're running with more than one device in a filesystem.
-btrfs device scan
-mount $DISK /green-btrfs
+mount_green_btrfs
